2c388633871e3ffbaf93036d652fdd52fafdf359
sasaki <skyo.97.admj@gmail.com>
2021-06-03 14:02:38 +0900

    edit

--- pages/index.vue
+++ pages/index.vue
@@ -10,35 +10,47 @@
 import { defineComponent, ref, onMounted } from '@nuxtjs/composition-api';
 import liff from '@line/liff';
 
-declare global {
-  interface Window {
-    liff: any;
-  }
-}
-window.liff = window.liff || {};
+// declare global {
+//   interface Window {
+//     liff: any;
+//   }
+// }
+// window.liff = window.liff || {};
 
 export default defineComponent({
   setup() {
     const name = ref<string>('');
 
     onMounted(async () => {
-      await window.liff.init({
+      await liff.init({
         liffId: process.env.LIFF_ID as string,
       });
+      // await window.liff.init({
+      //   liffId: process.env.LIFF_ID as string,
+      // });
     });
 
     const addName = async (): Promise<void> => {
       try {
-        await window.liff.sendMessages([
+        await liff.sendMessages([
           {
             type: 'text',
             text: name.value,
           },
         ]);
-        await window.liff.closeWindow();
+        // await window.liff.sendMessages([
+        //   {
+        //     type: 'text',
+        //     text: name.value,
+        //   },
+        // ]);
+        await liff.closeWindow();
+        // await window.liff.closeWindow();
       } catch (err: any) {
-        window.alert(err);
-        await window.liff.closeWindow();
+        alert(err);
+        // window.alert(err);
+        await liff.closeWindow();
+        // await window.liff.closeWindow();
       }
     };
 
